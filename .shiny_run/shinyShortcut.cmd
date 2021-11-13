cc158f46f53c38cae0b03e762b711f78feba5f3d
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2021-11-13 13:36:25 +0530

    Windows implementation

--- .shiny_run/shinyShortcut.cmd
+++ .shiny_run/shinyShortcut.cmd
@@ -1,2 +1,6 @@
+REM @echo off > NUL
+REM "C:\Program Files\R\R-4.1.1\bin\Rscript.exe" -e "shiny::runApp('C:/rgfsem/RGfSEM-master/RSfSEM.R', launch.browser = TRUE)"
+
 @echo off > NUL
-"C:\Program Files\R\R-4.1.1\bin\Rscript.exe" -e "shiny::runApp('C:/rgfsem/RGfSEM-master/RSfSEM.R', launch.browser = TRUE)"
+for /f "tokens=*" %%g in ('where Rscript') do (set rpath=%%g)
+"%rpath%" -e "shiny::runApp('D:/Work/Shiny/rsfsem/RSfSEM.R', launch.browser = TRUE)"
